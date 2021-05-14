693fe5fde95cf74eb508af39eba279ddf31e420b
Pradeep Gianchandani <pgianchandani@zipcar.com>
2021-05-14 14:36:03 +0530

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Pradeep Gianchandani on 14/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
